e70011e72ef109676c69232a43cf253b2998c6fb
Marcus Ramos <155693483+marcusramos2024@users.noreply.github.com>
2024-12-29 14:27:34 -0600

    Adding $ signs to products

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -5,7 +5,7 @@ function ProductCard({ product }) {
     <div className="product-card" data-category={product.category}>
       <img src={process.env.REACT_APP_STORAGE_BUCKET_URL + product.img_path} alt={product.name} />
       <h3>{product.name}</h3>
-      <p>{product.price}</p>
+      <p>${product.price}</p>
       <a href="#" className="buy-button">
         <i className="fas fa-shopping-cart"></i> Buy Now
       </a>
