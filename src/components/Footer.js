95c7e0560c427b10a18bad56ef6beaae7a8d2440
Marcus Ramos <155693483+marcusramos2024@users.noreply.github.com>
2024-12-29 14:47:33 -0600

    Removing unused footer icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,14 +7,8 @@ function Footer() {
       <div className="footer-content">
         <p>&copy; 2024 Sarah's Apparel. All rights reserved.</p>
         <div className="social-icons">
-          <a href="#">
-            <i className="fab fa-facebook-f"></i>
-          </a>
           <a href="https://www.instagram.com/sarah.franklin13?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">
             <i className="fab fa-instagram"></i>
-          </a>
-          <a href="#">
-            <i className="fab fa-twitter"></i>
           </a>
         </div>
       </div>
