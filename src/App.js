38e92214e120c4bfc687a973437ba61d62e804bf
Marcus Ramos <155693483+marcusramos2024@users.noreply.github.com>
2025-02-08 18:02:22 -0500

    Adding analytics

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // App.js
 import React, { useState, useEffect } from "react";
+import { Analytics } from "@vercel/analytics/react"
 import "./style.css";
 
 import Navbar from "./components/Navbar";
